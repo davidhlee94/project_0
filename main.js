b7122f5cc6299bf3cdcfe23d1dc8c08bec5e22dc
davidhlee94 <davidhlee94@gmail.com>
2022-11-10 14:01:16 -0800

    added prompt for Tamagotchi name

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 
+
+
 const hunger = document.querySelector('#hunger')
 const sleepiness = document.querySelector('#sleepiness')
 const boredome = document.querySelector('#boredome')
 const age = document.querySelector('#age')
 
+
+//Constants for the status numbers
 const hungerNumber = document.querySelector('.hunger-number')
 const sleepinessNumber = document.querySelector('.sleepiness-number')
 const boredomeNumber = document.querySelector('.boredome-number')
 const ageNumber = document.querySelector('.age-number')
 
-
-
+//Tamagotchi's name
+let tamagotchiName = prompt("Please name your Tamagotchi");
