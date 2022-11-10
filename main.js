5289df46ef8ec165f568c14e2ff02dfebbdd3ca4
davidhlee94 <davidhlee94@gmail.com>
2022-11-10 13:21:57 -0800

    added buttons and input

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ const sleepiness = document.querySelector('#sleepiness')
 const boredome = document.querySelector('#boredome')
 const age = document.querySelector('#age')
 
-setInterval
-
-
-
+const hungerNumber = document.querySelector('.hunger-number')
+const sleepinessNumber = document.querySelector('.sleepiness-number')
+const boredomeNumber = document.querySelector('.boredome-number')
+const ageNumber = document.querySelector('.age-number')
 
 
 
