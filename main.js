8f9d5c41890dce24d8df7d836ea6e1b6e9ba3db7
davidhlee94 <davidhlee94@gmail.com>
2022-11-10 14:32:59 -0800

    added some constants and start game info

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 
 
-
+//Constants for the status
 const hunger = document.querySelector('#hunger')
 const sleepiness = document.querySelector('#sleepiness')
 const boredome = document.querySelector('#boredome')
 const age = document.querySelector('#age')
 
 
-//Constants for the status numbers
-const hungerNumber = document.querySelector('.hunger-number')
-const sleepinessNumber = document.querySelector('.sleepiness-number')
-const boredomeNumber = document.querySelector('.boredome-number')
-const ageNumber = document.querySelector('.age-number')
+//Constants for buttons
+const feedBtn = document.querySelector('.feed')
+const lightBtn = document.querySelector('.light')
+const playBtn = document.querySelector('.play')
+
 
 //Tamagotchi's name
 let tamagotchiName = prompt("Please name your Tamagotchi");
+
+//Start game
+const hungerStart = 10;
+const sleepinessStart = 10;
+const boredomeStart = 10;
+const ageStart = 1
