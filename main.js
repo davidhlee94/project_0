72aef8cc7a87b4d7dba8a8bb9391e8fc704cc1f7
davidhlee94 <davidhlee94@gmail.com>
2022-11-10 17:28:52 -0800

    added the naming function

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
 
 //Constants for the status
-const hunger = document.querySelector('#hunger')
-const sleepiness = document.querySelector('#sleepiness')
-const boredome = document.querySelector('#boredome')
-const age = document.querySelector('#age')
+const hungerStat = document.querySelector('#hunger-stat')
+const sleepinessStat = document.querySelector('#sleepiness-stat')
+const boredomeStat = document.querySelector('#boredome-stat')
+const ageStat = document.querySelector('#age-stat')
 
 
 //Constants for buttons
@@ -14,10 +14,30 @@ const playBtn = document.querySelector('.play')
 
 
 //Tamagotchi's name
-let tamagotchiName = prompt("Please name your Tamagotchi");
+const nameBtn = document.querySelector('#name-btn');
+console.log(nameBtn);
+
+nameBtn.addEventListener('click', () => {
+    let nameInput = document.querySelector('#input');
+    let message = document.querySelector('#message');
+    message.innerText = "Hi my name is " + nameInput.value + "!";
+});
+
 
 //Start game
 const hungerStart = 10;
 const sleepinessStart = 10;
 const boredomeStart = 10;
 const ageStart = 1
+
+
+
+
+
+//TamaGucci function
+function tamagucci(){
+    this.hunger = hungerStart;
+    this.sleepiness = sleepinessStart;
+    this.boredome = boredomeStart;
+    this.age = ageStart;
+}
